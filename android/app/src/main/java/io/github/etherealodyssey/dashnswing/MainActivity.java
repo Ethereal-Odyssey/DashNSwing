634c8ad7248dde39ae02c6f1e2155894c0da44a1
SHIN-MK <dnu8894@gmail.com>
2024-01-03 00:20:16 +0900

    안드로이드 생성

---
+++ android/app/src/main/java/io/github/etherealodyssey/dashnswing/MainActivity.java
@@ -0,0 +1,5 @@
+package io.github.etherealodyssey.dashnswing;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,12 +1,12 @@
-import { CapacitorConfig } from '@capacitor/cli';
+import { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: 'io.github.etherealodyssey.dashnswing',
-  appName: 'ethereal-odyssey',
-  webDir: 'dist',
+  appId: "io.github.etherealodyssey.dashnswing",
+  appName: "Dash 'N' Swing",
+  webDir: "dist",
   server: {
-    androidScheme: 'https'
-  }
+    androidScheme: "https",
+  },
 };
 
 export default config;
